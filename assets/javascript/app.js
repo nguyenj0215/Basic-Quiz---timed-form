ed2426539e0b0346980a58684db70c17f1e86ec7
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-09-04 21:03:29 -0700

    added reset button that unselects radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Default: have page one showing for 30 seconds and page two hidden    
+// Default: have page one showing for 30 seconds and page two hidden ---------------
 $(".pageTwo").hide()
 
 function pageOneTimer() {
@@ -6,25 +6,28 @@ function pageOneTimer() {
 }
 pageOneTimer()
 
-// Make page one hidden and make page two visible
+// Make page one hidden and make page two visible----------------------------------
 function pageTwo() {
     $(".pageOne").hide()
     $(".pageTwo").show()
 }
 
-// Counter for time
+// Counter for time----------------------------------------------------------------
 var timerInt;
-var time = 15;
+var time = 25;
+var score = 0;
 
 function counterDown() {
     if (time > 0) {
         time--;
         $(".timerText").text(time)
     } else {
-        $(".timerTextHeader").html("Times up! Here are the correct answers!")
+        $(".forPage1").hide()
+        $(".forPage2").show()
 
         clearInterval(timerInt)
 
+        //Checking radio buttons for correct box checked-----------------------------------
         if ($('input[name="q1Ans"]:checked').val() == "tor") {
             score++;
         }
@@ -48,5 +51,62 @@ function startTimer() {
 }
 startTimer()
 
+
+//Restart game function------------------------------------------------------------
+$(".restart").on("click", function restart() {
+
+    score = 0
+    time = 25
+
+    $(".pageOne").show()
+    $(".pageTwo").hide()
+
+    $(".forPage1").show()
+    $(".forPage2").hide()
+    $(".timerText").text("25")
+
+    function pageOneTimer() {
+        setTimeout(pageTwo, 25 * 1000)
+    }
+    pageOneTimer()
+
+    function startTimer() {
+        timerInt = setInterval(counterDown, 1000)
+    }
+    startTimer()
+
+    var timerInt;
+var time = 25;
 var score = 0;
 
+function counterDown() {
+    if (time > 0) {
+        time--;
+        $(".timerText").text(time)
+    } else {
+        $(".forPage1").hide()
+        $(".forPage2").show()
+
+        clearInterval(timerInt)
+
+        if ($('input[name="q1Ans"]:checked').val() == "tor") {
+            score++;
+        }
+        if ($('input[name="q2Ans"]:checked').val() == "ne") {
+            score++;
+        }
+        if ($('input[name="q3Ans"]:checked').val() == "oak") {
+            score++;
+        }
+        if ($('input[name="q4Ans"]:checked').val() == "tex") {
+            score++;
+        }
+
+        $(".score").text(score)
+
+    }
+}
+
+$(".radioBtn").prop('checked', false);
+
+})
