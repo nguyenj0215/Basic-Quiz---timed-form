7fba5d7e65d0b63045a11e8783d180bff994cf37
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-09-06 09:55:49 -0700

    unanswered display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,116 +1,154 @@
 $(document).ready(function () {
 
-// Default: have page one showing for 30 seconds and page two hidden ---------------
-$(".pageTwo").hide()
+    // Default: have page one showing for 30 seconds and page two hidden ---------------
+    $(".pageTwo").hide()
 
-var stopPageTimeout;
+    var stopPageTimeout;
 
-function pageOneTimer() {
+    function pageOneTimer() {
 
-    stopPageTimeout = setTimeout(pageTwo, 25 * 1000)
-}
-
-pageOneTimer()
-
-// Make page one hidden and make page two visible----------------------------------
-function pageTwo() {
-
-    $(".pageOne").hide()
-    $(".pageTwo").show()
-}
-
-// Counter for time----------------------------------------------------------------
-var timerInt;
-var time = 25;
-var score = 0;
-
-function counterDown() {
-
-    if (time > 0) {
-        time--;
-        $(".timerText").text(time)
+        stopPageTimeout = setTimeout(pageTwo, 25 * 1000)
     }
-    else {
-        $(".forPage1").hide()
-        $(".forPage2").show()
-
-        clearInterval(timerInt)
-
-        //Checking radio buttons for correct box checked-------------------------------------
-        if ($('input[name="q1Ans"]:checked').val() == "tor") {
-            score++;
-        }
-        if ($('input[name="q2Ans"]:checked').val() == "ne") {
-            score++;
-        }
-        if ($('input[name="q3Ans"]:checked').val() == "oak") {
-            score++;
-        }
-        if ($('input[name="q4Ans"]:checked').val() == "tex") {
-            score++;
-        }
-
-        $(".score").text(score)
-
-    }
-}
-
-function startTimer() {
-
-    timerInt = setInterval(counterDown, 1000)
-}
-startTimer()
-
-//Submit button function -----------------------------------------------
-$(".submit").on("click", function submit() {
-
-    clearInterval(timerInt)
-    clearTimeout(stopPageTimeout)
-
-    $(".pageOne").hide()
-    $(".pageTwo").show()
-
-    if ($('input[name="q1Ans"]:checked').val() == "tor") {
-        score++;
-    }
-    if ($('input[name="q2Ans"]:checked').val() == "ne") {
-        score++;
-    }
-    if ($('input[name="q3Ans"]:checked').val() == "oak") {
-        score++;
-    }
-    if ($('input[name="q4Ans"]:checked').val() == "tex") {
-        score++;
-    }
-
-    $(".score").text(score)
-})
-
-
-
-//Restart game function------------------------------------------------------------
-$(".restart").on("click", function restart() {
-
-    clearInterval(timerInt)
-    clearTimeout(stopPageTimeout)
 
     pageOneTimer()
 
+    // Make page one hidden and make page two visible----------------------------------
+    function pageTwo() {
+
+        $(".pageOne").hide()
+        $(".pageTwo").show()
+    }
+
+    // Counter for time----------------------------------------------------------------
+    var timerInt;
+    var time = 25;
+    var correct = 0;
+    var count = 0;
+
+    function counterDown() {
+
+        if (time > 0) {
+            time--;
+            $(".timerText").text(time)
+        }
+        else {
+            $(".forPage1").hide()
+            $(".forPage2").show()
+
+            clearInterval(timerInt)
+
+            //Checking radio buttons for correct box checked-------------------------------------
+            if ($('input[name="q1Ans"]:checked').val() == "tor") {
+                correct++;
+                count++
+            }
+            else if ($('input[name="q1Ans"]:checked').val() !== "tor") {
+                count++;
+            }
+            //need to check each questions to see if user left all boxes blank
+            if ($('input[name="q2Ans"]:checked').val() == "ne") {
+                correct++;
+                count++
+            }
+            else if ($('input[name="q2Ans"]:checked').val() !== "tor") {
+                count++;
+            }
+            if ($('input[name="q3Ans"]:checked').val() == "oak") {
+                correct++;
+                count++
+            }
+            else if ($('input[name="q3Ans"]:checked').val() !== "tor") {
+                count++;
+            }
+            if ($('input[name="q4Ans"]:checked').val() == "tex") {
+                correct++;
+                count++
+            }
+            else if ($('input[name="q4Ans"]:checked').val() !== "tor") {
+                count++
+            }
+
+            $(".score").text(correct)
+            $(".unanswered").text(count)
+
+        }
+    }
+
+    function startTimer() {
+
+        timerInt = setInterval(counterDown, 1000)
+    }
+
     startTimer()
 
-    counterDown()
+    //Submit button function -----------------------------------------------
+    $(".submit").on("click", function submit() {
 
-    score = 0
-    time = 25
+        clearInterval(timerInt)
+        clearTimeout(stopPageTimeout)
 
-    $(".pageOne").show()
-    $(".pageTwo").hide()
+        $(".pageOne").hide()
+        $(".pageTwo").show()
 
-    $(".forPage1").show()
-    $(".forPage2").hide()
-    $(".timerText").text("25")
+        if ($('input[name="q1Ans"]:checked').val() == "tor") {
+            correct++;
+            count++
+        }
+        else if ($('input[name="q1Ans"]:checked').val() !== "tor") {
+            count++;
+        }
+        //need to check each questions to see if user left all boxes blank
+        if ($('input[name="q2Ans"]:checked').val() == "ne") {
+            correct++;
+            count++
+        }
+        else if ($('input[name="q2Ans"]:checked').val() !== "tor") {
+            count++;
+        }
+        if ($('input[name="q3Ans"]:checked').val() == "oak") {
+            correct++;
+            count++
+        }
+        else if ($('input[name="q3Ans"]:checked').val() !== "tor") {
+            count++;
+        }
+        if ($('input[name="q4Ans"]:checked').val() == "tex") {
+            correct++;
+            count++
+        }
+        else if ($('input[name="q4Ans"]:checked').val() !== "tor") {
+            count++
+        }
 
-    $(".radioBtn").prop('checked', false);
+        $(".score").text(correct)
+        $(".unanswered").text(count)
+    })
 
-})
+
+
+    //Restart game function------------------------------------------------------------
+    $(".restart").on("click", function restart() {
+
+        clearInterval(timerInt)
+        clearTimeout(stopPageTimeout)
+
+        pageOneTimer()
+
+        startTimer()
+
+        counterDown()
+
+        correct = 0
+        time = 25
+
+        $(".pageOne").show()
+        $(".pageTwo").hide()
+
+        $(".forPage1").show()
+        $(".forPage2").hide()
+        $(".timerText").text("25")
+
+        $(".radioBtn").prop('checked', false);
+
+    })
 })
